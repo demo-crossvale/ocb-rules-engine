7f20d6d96b0e6d9356f4039c27fb70ef4990aee8
Alex <31018251+afeiszli@users.noreply.github.com>
2019-04-03 23:02:21 -0500

    Update SpringBootApp.java

--- src/main/java/com/SpringBootApp.java
+++ src/main/java/com/SpringBootApp.java
@@ -5,7 +5,9 @@ import org.kie.api.runtime.KieContainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class SpringBootApp {
 
